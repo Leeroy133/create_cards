9fe417a67f7eefbde01c225ef5505113818a61a9
LaraxInc <nikita.kozytenko@gmail.com>
2024-10-10 22:31:58 +0300

    Update create_cards.py

--- create_cards.py
+++ create_cards.py
@@ -3,163 +3,111 @@ import faker
 import random
 import os
 
+from faker import Faker
+
 
 os.makedirs('cards', mode=0o777, exist_ok=False)
 
 
-from faker import Faker
-
-
-skills_list = ['Стремительный прыжок', 
-'Электрический выстрел', 
-'Ледяной удар',
-'Стремительный удар',
-'Кислотный взгляд',
-'Тайный побег',
-'Ледяной выстрел',
-'Огненный заряд']
+letters_mapping = {
+    'а': 'а͠',
+    'б': 'б̋',
+    'в': 'в͒͠',
+    'г': 'г͒͠',
+    'д': 'д̋',
+    'е': 'е͠',
+    'ё': 'ё͒͠',
+    'ж': 'ж͒',
+    'з': 'з̋̋͠',
+    'и': 'и',
+    'й': 'й͒͠',
+    'к': 'к̋̋',
+    'л': 'л̋͠',
+    'м': 'м͒͠',
+    'н': 'н͒',
+    'о': 'о̋',
+    'п': 'п̋͠',
+    'р': 'р̋͠',
+    'с': 'с͒',
+    'т': 'т͒',
+    'у': 'у͒͠',
+    'ф': 'ф̋̋͠',
+    'х': 'х͒͠',
+    'ц': 'ц̋',
+    'ч': 'ч̋͠',
+    'ш': 'ш͒͠',
+    'щ': 'щ̋',
+    'ъ': 'ъ̋͠',
+    'ы': 'ы̋͠',
+    'ь': 'ь̋',
+    'э': 'э͒͠͠',
+    'ю': 'ю̋͠',
+    'я': 'я̋',
+    'А': 'А͠',
+    'Б': 'Б̋',
+    'В': 'В͒͠',
+    'Г': 'Г͒͠',
+    'Д': 'Д̋',
+    'Е': 'Е',
+    'Ё': 'Ё͒͠',
+    'Ж': 'Ж͒',
+    'З': 'З̋̋͠',
+    'И': 'И',
+    'Й': 'Й͒͠',
+    'К': 'К̋̋',
+    'Л': 'Л̋͠',
+    'М': 'М͒͠',
+    'Н': 'Н͒',
+    'О': 'О̋',
+    'П': 'П̋͠',
+    'Р': 'Р̋͠',
+    'С': 'С͒',
+    'Т': 'Т͒',
+    'У': 'У͒͠',
+    'Ф': 'Ф̋̋͠',
+    'Х': 'Х͒͠',
+    'Ц': 'Ц̋',
+    'Ч': 'Ч̋͠',
+    'Ш': 'Ш͒͠',
+    'Щ': 'Щ̋',
+    'Ъ': 'Ъ̋͠',
+    'Ы': 'Ы̋͠',
+    'Ь': 'Ь̋',
+    'Э': 'Э͒͠͠',
+    'Ю': 'Ю̋͠',
+    'Я': 'Я̋',
+    ' ': ' '
+}
+skills_list = [
+        'Стремительный прыжок',
+        'Электрический выстрел',
+        'Ледяной удар',
+        'Стремительный удар',
+        'Кислотный взгляд',
+        'Тайный побег',
+        'Ледяной выстрел',
+        'Огненный заряд'
+]
 runic_skills = []
 
 
-for i in range(len(skills_list)):
-    runic_skills.append(skills_list[i])
-    update_skill=runic_skills[i].replace('а', 'а͠')
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('б', 'б̋')
-    runic_skills[i]=update_skill 
-    update_skill=runic_skills[i].replace('в', 'в͒͠')
-    runic_skills[i]=update_skill 
-    update_skill=runic_skills[i].replace('г', 'г͒͠') 
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('д', 'д̋')
-    runic_skills[i]=update_skill 
-    update_skill=runic_skills[i].replace('е', 'е͠')
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('ё', 'ё͒͠') 
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('ж', 'ж͒') 
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('з', 'з̋̋͠')
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('и', 'и') 
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('й', 'й͒͠') 
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('к', 'к̋̋')
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('л', 'л̋͠') 
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('м', 'м͒͠') 
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('н', 'н͒')
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('о', 'о̋') 
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('п', 'п̋͠') 
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('р', 'р̋͠')
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('с', 'с͒') 
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('т', 'т͒') 
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('у', 'у͒͠')
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('ф', 'ф̋̋͠') 
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('х', 'х͒͠') 
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('ц', 'ц̋')
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('ч', 'ч̋͠') 
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('ш', 'ш͒͠') 
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('щ', 'щ̋')
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('ъ', 'ъ̋͠') 
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('ы', 'ы̋͠') 
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('ь', 'ь̋')
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('э', 'э͒͠͠') 
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('ю', 'ю̋͠')
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('я', 'я̋')
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('А', 'А͠') 
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('Б', 'Б̋') 
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('В', 'В͒͠')
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('Г', 'Г͒͠') 
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('Д', 'Д̋') 
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('Е', 'Е')
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('Ё', 'Ё͒͠') 
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('Ж', 'Ж͒') 
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('З', 'З̋̋͠')
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('И', 'И') 
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('Й', 'Й͒͠') 
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('К', 'К̋̋')
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('Л', 'Л̋͠') 
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('М', 'М͒͠') 
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('Н', 'Н͒')
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('О', 'О̋') 
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('П', 'П̋͠') 
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('Р', 'Р̋͠')
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('С', 'С͒') 
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('Т', 'Т͒') 
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('У', 'У͒͠')
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('Ф', 'Ф̋̋͠') 
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('Х', 'Х͒͠') 
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('Ц', 'Ц̋')
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('Ч', 'Ч̋͠') 
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('Ш', 'Ш͒͠') 
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('Щ', 'Щ̋')
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('Ъ', 'Ъ̋͠') 
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('Ы', 'Ы̋͠') 
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('Ь', 'Ь̋')
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('Э', 'Э͒͠͠') 
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('Ю', 'Ю̋͠') 
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace('Я', 'Я̋')
-    runic_skills[i]=update_skill
-    update_skill=runic_skills[i].replace(' ', ' ')
-    runic_skills[i]=update_skill
-
-
 def main():
+    skills_list = [
+            'Стремительный прыжок',
+            'Электрический выстрел',
+            'Ледяной удар',
+            'Стремительный удар',
+            'Кислотный взгляд',
+            'Тайный побег',
+            'Ледяной выстрел',
+            'Огненный заряд'
+    ]
+    runic_skills = []
+    for i in range(len(skills_list)):
+        runic_skills.append(skills_list[i])
+        for s in range(len(list(letters_mapping.keys()))):
+            update_skill=runic_skills[i].replace(list(letters_mapping.keys())[s], list(letters_mapping.values())[s])
+            runic_skills[i]=update_skill
     for number in range(10):
         pers_skills = random.sample(runic_skills, 3)
         fake = Faker("ru_RU")
@@ -177,11 +125,8 @@ def main():
             'skill_2': pers_skills[1],
             'skill_3': pers_skills[2]
         }
-        
-        
         file_operations.render_template('charsheet.svg', 'cards/processed_charsheet_{}.svg'.format(number), context)
 
 
 if __name__ == '__main__':
-    main()        
-
+    main()
